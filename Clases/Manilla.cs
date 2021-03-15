cc7e157beb6996042efc78d2cad11055c5a85cea
geraldinnesd <71853387+geraldinnesd@users.noreply.github.com>
2021-03-15 17:59:25 -0500

    Agregar archivos de proyecto.

---
+++ Clases/Manilla.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parque_TeamWork.Clases
+{
+    class Manilla
+    {
+        private byte id_interno;
+        private ushort nro_puntos;
+        private float dinero;
+
+
+        //Constructor
+        public Manilla(byte id_interno, ushort nro_puntos, float dinero)
+        {
+            Id_interno = id_interno;
+            Nro_puntos = nro_puntos;
+            Dinero = dinero;
+        }
+
+        //Accesor
+
+        public byte Id_interno { get => id_interno; set => id_interno = value; }
+        public ushort Nro_puntos { get => nro_puntos; set => nro_puntos = value; }
+        public float Dinero { get => dinero; set => dinero = value; }
+    }
+}
---
+++ Clases/Taquilla.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parque_TeamWork.Clases
+{
+    class Taquilla
+    {
+        private byte id;
+        private byte nro_manillas_taquilla;
+
+        //Constructor
+
+        public Taquilla(byte id, byte nro_manillas_taquilla)
+        {
+            this.id = id;
+            this.nro_manillas_taquilla = nro_manillas_taquilla;
+        }
+        public byte Id { get => id; set => id = value; }
+        public byte Nro_manillas_taquilla { get => nro_manillas_taquilla; set => nro_manillas_taquilla = value; }
+    }
+}
